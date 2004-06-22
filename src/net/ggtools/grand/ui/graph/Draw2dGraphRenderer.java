8d37a6296835dbcf5e6593b0ae628625ebc8f0a2
Christophe Labouisse <ggtools@labouisse.org>
2004-06-22 22:01:44 +0000

    * Compute the actual bounding box of the connections as getBounds() do not return correct values (ticket #9),  * Add a margin on the right & bottom borders to compensate for JzGraph top & left margin.

--- src/net/ggtools/grand/ui/graph/Draw2dGraphRenderer.java
+++ src/net/ggtools/grand/ui/graph/Draw2dGraphRenderer.java
@@ -46,11 +46,14 @@ import org.eclipse.draw2d.Cursors;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.LineBorder;
+import org.eclipse.draw2d.MarginBorder;
 import org.eclipse.draw2d.MidpointLocator;
 import org.eclipse.draw2d.PolygonDecoration;
 import org.eclipse.draw2d.PolylineConnection;
 import org.eclipse.draw2d.PositionConstants;
+import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.swt.graphics.Color;
 
 import sf.jzgraph.IDotGraph;
@@ -69,6 +72,11 @@ public class Draw2dGraphRenderer implements DotGraphAttributes {
     public Draw2dGraph render(IDotGraph dotGraph) {
         if (log.isDebugEnabled()) log.debug("Rendering Draw2d Graph");
         final Draw2dGraph contents = new Draw2dGraph();
+        // Add a margin on the right & bottom edge.
+        // The 24 is a magic number which seems to be the same margin JzGraph is
+        // applying on the top & left edges.
+        // FIXME find out the real dot margin.
+        contents.setBorder(new MarginBorder(0,0,24,24));
 
         for (Iterator iter = dotGraph.allVertices().iterator(); iter.hasNext();) {
             final IVertex node = (IVertex) iter.next();
@@ -84,34 +92,20 @@ public class Draw2dGraphRenderer implements DotGraphAttributes {
     }
 
     /**
-     * Builds a figure for the given edge and adds it to contents
-     * 
-     * @param contents
-     *            the parent figure to add the edge to
-     * @param edge
-     *            the edge
+     * @param coords
+     * @param bends
+     * @param min
+     * @param max
      */
-    private void buildEdgeFigure(final IFigure contents, final IEdge edge) {
-        if (log.isTraceEnabled())
-                log.trace("Building edge from " + edge.getTail().getName() + " to "
-                        + edge.getHead().getName());
-        final DotRoute route = (DotRoute) edge.getAttr(POSITION_ATTR);
-
-        String name = edge.getName();
-        if ("".equals(name)) name = null;
-
-        final PolylineConnection conn = createConnectionFromRoute(contents, name, route);
-
-        if (edge.getAttr(DRAW2DFGCOLOR_ATTR) != null) {
-            conn.setForegroundColor((Color) edge.getAttr(DRAW2DFGCOLOR_ATTR));
-        }
-
-        final PolygonDecoration dec = new PolygonDecoration();
-        conn.setTargetDecoration(dec);
-
-        conn.setToolTip(new LinkTooltip(edge));
-        conn.setCursor(Cursors.HAND);
-        contents.add(conn, conn.getBounds());
+    private final void addBendPoint(final float[] coords, final ArrayList bends, final Point min,
+            final Point max) {
+        final int x = (int) coords[0];
+        final int y = (int) coords[1];
+        bends.add(new AbsoluteBendpoint(x, y));
+        if (x < min.x) min.x = x;
+        if (x > max.x) max.x = x;
+        if (y < min.y) min.y = y;
+        if (y > max.y) max.y = y;
     }
 
     /**
@@ -120,11 +114,14 @@ public class Draw2dGraphRenderer implements DotGraphAttributes {
      * @param route
      * @return
      */
-    private PolylineConnection createConnectionFromRoute(final IFigure contents, final String name,
+    private PolylineConnection addConnectionFromRoute(final IFigure contents, final String name,
             final DotRoute route) {
         final float[] coords = new float[6];
         final ArrayList bends = new ArrayList();
         boolean isFirstPoint = true;
+        final Point min = new Point(Integer.MAX_VALUE, Integer.MAX_VALUE);
+        final Point max = new Point(Integer.MIN_VALUE, Integer.MIN_VALUE);
+
         for (final PathIterator ite = new FlatteningPathIterator(route.getPath().getPathIterator(
                 new AffineTransform()), PATH_ITERATOR_FLATNESS); !ite.isDone(); ite.next()) {
             final int segType = ite.currentSegment(coords);
@@ -132,7 +129,7 @@ public class Draw2dGraphRenderer implements DotGraphAttributes {
             switch (segType) {
             case PathIterator.SEG_MOVETO:
                 if (isFirstPoint) {
-                    bends.add(new AbsoluteBendpoint((int) coords[0], (int) coords[1]));
+                    addBendPoint(coords, bends, min, max);
                 }
                 else {
                     log.error("Got SEG_MOVETO after first point");
@@ -140,7 +137,7 @@ public class Draw2dGraphRenderer implements DotGraphAttributes {
                 break;
 
             case PathIterator.SEG_LINETO:
-                bends.add(new AbsoluteBendpoint((int) coords[0], (int) coords[1]));
+                addBendPoint(coords, bends, min, max);
                 break;
 
             default:
@@ -151,6 +148,7 @@ public class Draw2dGraphRenderer implements DotGraphAttributes {
             isFirstPoint = false;
         }
 
+        Rectangle bounds = new Rectangle(min, max);
         final PolylineConnection conn = new PolylineConnection();
 
         final Point sourcePoint = (Point) bends.remove(0);
@@ -180,9 +178,46 @@ public class Draw2dGraphRenderer implements DotGraphAttributes {
             label.setFont(Application.getInstance().getFont(Application.LINK_FONT));
             final ConnectionLocator locator = new MidpointLocator(conn, bends.size() / 2);
             locator.setRelativePosition(PositionConstants.CENTER);
+            // Includes the label in the connnection bounds.
+            // Worth cas scenario, the label is on the connection edge.
+            final Dimension labelSize = label.getPreferredSize();
+            bounds.expand(labelSize.width, labelSize.height);
             conn.add(label, locator);
         }
+
+        contents.add(conn, bounds);
+
         return conn;
+    }
+
+    /**
+     * Builds a figure for the given edge and adds it to contents
+     * 
+     * @param contents
+     *            the parent figure to add the edge to
+     * @param edge
+     *            the edge
+     */
+    private void buildEdgeFigure(final IFigure contents, final IEdge edge) {
+        if (log.isTraceEnabled())
+                log.trace("Building edge from " + edge.getTail().getName() + " to "
+                        + edge.getHead().getName());
+        final DotRoute route = (DotRoute) edge.getAttr(POSITION_ATTR);
+
+        String name = edge.getName();
+        if ("".equals(name)) name = null;
+
+        final PolylineConnection conn = addConnectionFromRoute(contents, name, route);
+
+        if (edge.getAttr(DRAW2DFGCOLOR_ATTR) != null) {
+            conn.setForegroundColor((Color) edge.getAttr(DRAW2DFGCOLOR_ATTR));
+        }
+
+        final PolygonDecoration dec = new PolygonDecoration();
+        conn.setTargetDecoration(dec);
+
+        conn.setToolTip(new LinkTooltip(edge));
+        conn.setCursor(Cursors.HAND);
     }
 
     /**
@@ -229,7 +264,7 @@ public class Draw2dGraphRenderer implements DotGraphAttributes {
      */
     private PolylineConnection createBusConnexion(Draw2dGraph contents, IVertex node, Color color,
             String busId, String busLabel) {
-        final PolylineConnection conn = createConnectionFromRoute(contents, null, (DotRoute) node
+        final PolylineConnection conn = addConnectionFromRoute(contents, null, (DotRoute) node
                 .getAttr(busId));
         conn.setForegroundColor(color);
         contents.add(conn, conn.getBounds());
