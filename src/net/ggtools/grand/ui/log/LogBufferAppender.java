09c534d3903501463c7d70ecacf6b09bb4ae17fd
Christophe Labouisse <ggtools@labouisse.org>
2005-02-06 20:44:35 +0000

    Added obsolete LogBufferAppender class to keep a version of it.

---
+++ src/net/ggtools/grand/ui/log/LogBufferAppender.java
@@ -0,0 +1,64 @@
+// $Id$
+/*
+ * ====================================================================
+ * Copyright (c) 2002-2004, Christophe Labouisse All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.ggtools.grand.ui.log;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+
+/**
+ * Log4J appender to store the logs in the LogEventBufferImpl.
+ * 
+ * @author Christophe Labouisse
+ */
+public class LogBufferAppender extends AppenderSkeleton implements Appender {
+
+    /*
+     * (non-Javadoc)
+     * @see org.apache.log4j.AppenderSkeleton#append(org.apache.log4j.spi.LoggingEvent)
+     */
+    protected void append(LoggingEvent event) {
+        final LogEventBuffer instance = LogEventBufferImpl.getInstance();
+        //if (instance != null) instance.logEvent(event);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.apache.log4j.Appender#close()
+     */
+    public void close() {
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.apache.log4j.Appender#requiresLayout()
+     */
+    public boolean requiresLayout() {
+        return false;
+    }
+}
