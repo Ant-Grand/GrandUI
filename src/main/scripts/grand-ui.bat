279673104311d6cf263fd4241944b708c8d728ea
twogee <g.grigelionis@gmail.com>
2019-12-15 17:09:28 +0100

    Change Java baseline to 8, upgrade to Eclipse 4.12 (2019-06): 64-bit only

--- src/main/scripts/grand-ui.bat
+++ src/main/scripts/grand-ui.bat
@@ -1,13 +1,12 @@
 @set BASEDIR=%~dp0
 
-@set ARCH=x86_64
 @java -version 2>&1 | findstr 64 >nul
-@if ERRORLEVEL 1 set ARCH=x86
+@if ERRORLEVEL 1 (echo "SWT 4.10+ requires 64-bit Java" & exit /b 1)
 
 @for /f tokens^=2^ delims^=.-+_^" %%j in ('java -fullversion 2^>^&1') do set "java.major=%%j"
 if %java.major%==1 (
-    start javaw -Djava.ext.dirs=%BASEDIR%lib;%BASEDIR%lib\win32\%ARCH% -jar %BASEDIR%lib\grand-ui.jar
+    start javaw -Djava.ext.dirs=%BASEDIR%lib;%BASEDIR%lib\win32 -jar %BASEDIR%lib\grand-ui.jar
 ) else (
-    start javaw -p %BASEDIR%lib;%BASEDIR%lib\win32\%ARCH% -m grand.ui
+    start javaw -p %BASEDIR%lib;%BASEDIR%lib\win32 -m grand.ui
 )
 @exit
